d71b24e1e5019d0b6a92c5886bb1ca325db34e21
Ernesto914 <ernestolupicino914@gmail.com>
2024-09-21 15:30:03 +0200

    Esercizio 2.1

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    float numero1, numero2, media;
+    printf("inserisci il primo numero: ");
+    scanf("%f", &numero1);
+    printf("inserisci il secondo numero: ");
+    scanf("%f", &numero2);
+    media = (numero1 + numero2) / 2;
+    printf ("media = %f",media);
+}
+
